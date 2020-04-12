045a7781f559c287fd122daa2a50d269df3458be
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-11 18:36:45 -0700

    review notification methods -- passed

--- Pomodoro/FirstViewController.swift
+++ Pomodoro/FirstViewController.swift
@@ -18,6 +18,7 @@ class FirstViewController: UIViewController {
         print("Removed Observer")
 
         NotificationCenter.default.removeObserver(self, name: Notification.Name("receivedNotification"), object: nil)
+               
     }
     
     override func viewDidLoad() {
